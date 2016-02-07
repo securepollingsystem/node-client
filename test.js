f31d31bbdddb6e400e50d431b4a570cbaa859c7a
Karissa McKelvey <krmckelv@gmail.com>
2016-02-06 22:26:03 -0800

    take cmdline args

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 var SPSClient = require('./')
+var msg = process.argv[2] || 'I love ice cream'
 
 var client = SPSClient()
 client.generateKeypair()
 var regSig = 'iamregistrar'
 
-client.screed('I love ice cream', regSig, function (err, screed) {
+client.screed(msg, regSig, function (err, screed) {
   if (err) throw err
   console.log(screed)
 })
