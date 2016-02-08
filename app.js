7a1719d77c063e704e4d58692f310826924ac96a
Jay Graber <graber.jay@gmail.com>
2016-02-07 18:44:07 -0800

    Basic app with form not yet posting

---
+++ app.js
@@ -0,0 +1,31 @@
+var SPSClient = require('./');
+var client = SPSClient()
+client.generateKeypair()
+var regSig = 'iamregistrar'
+
+var express = require('express');
+var app = express();
+
+app.listen(3000)
+
+app.get('/', function(req, res){
+    res.render('index.jade');
+});
+
+app.get('/about', function(req, res){
+    res.render('about.jade');
+});
+
+
+// app.use(express.bodyParser());
+
+
+// app.post('/screed', function(req, res){
+        // var msg = req.body.screed
+//     res.send('Screed: ' + msg);
+
+//     // client.screed(msg, regSig, function (err, screed) {
+//     //   if (err) throw err
+//     //   console.log(screed)
+//     // })
+// });
